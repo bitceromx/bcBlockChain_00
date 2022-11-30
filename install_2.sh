03d4dd7fc4ab8988f309f4d6db66150af9d00888
root <jonyxiuh@gmail.com>
2022-11-30 17:49:40 -0600

    0007

--- install_2.sh
+++ install_2.sh
@@ -6,20 +6,23 @@ sudo apt-get install -y build-essential
 mkdir tmp
 cd tmp
 wget https://go.dev/dl/go1.15.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.18.7.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.15.linux-amd64.tar.gz
 cd ..
 rm -rf tmp
 
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
-echo "export PATH=$PATH:$GOPATH/bin" >> ~/.profile
 
 source ~/.profile
 go version
 
 cd utils/
+unzip main.zip
 mv fabric-main/gossip/comm/conn.go fabric-main/gossip/comm/conn.go-OLD
 cp conn.go fabric-main/gossip/comm/
 cd fabric-main/
+apt-get install -y build-essential
+go mod tidy
+go mod vendor
 make check-deps 
 
 ls gossip/comm/
